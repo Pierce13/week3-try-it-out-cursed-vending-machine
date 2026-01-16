a792cfaea8cdca201eb2b4ecff7744c0841ec35e
Pierce Scott <p.scott1396@gmail.com>
2026-01-16 09:40:04 -0700

    init

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/pscot/source/repos/week3-try-it-out-cursed-vending-machine
+# Build directory: C:/Users/pscot/source/repos/week3-try-it-out-cursed-vending-machine/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(main_testAreaOfCircle "main" "1")
+set_tests_properties(main_testAreaOfCircle PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/pscot/source/repos/week3-try-it-out-cursed-vending-machine/CMakeLists.txt;19;add_test;C:/Users/pscot/source/repos/week3-try-it-out-cursed-vending-machine/CMakeLists.txt;0;")
+add_test(main_testAddFractions "main" "2")
+set_tests_properties(main_testAddFractions PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/pscot/source/repos/week3-try-it-out-cursed-vending-machine/CMakeLists.txt;20;add_test;C:/Users/pscot/source/repos/week3-try-it-out-cursed-vending-machine/CMakeLists.txt;0;")
+add_test(main_testQuadraticFormula "main" "3")
+set_tests_properties(main_testQuadraticFormula PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/pscot/source/repos/week3-try-it-out-cursed-vending-machine/CMakeLists.txt;21;add_test;C:/Users/pscot/source/repos/week3-try-it-out-cursed-vending-machine/CMakeLists.txt;0;")
